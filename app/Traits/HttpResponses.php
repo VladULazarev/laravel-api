693c47962b23dc256d3a41a0ef6fbcd511cf9287
VladULazarev <getyourbestsite@gmail.com>
2023-03-17 08:51:12 +0300

    Edit file

--- app/Traits/HttpResponses.php
+++ app/Traits/HttpResponses.php
@@ -4,31 +4,23 @@ namespace App\Traits;
 
 trait HttpResponses
 {
-    /**
-     * The message, if the request was successful
-     *
-     * @return mixed
-     */
     protected function success($data, $message = null, $code = 200)
     {
         return response()->json([
             'status' => 'Request was successful.',
             'message' => $message,
-            'data' => $data
+            'data' => $data,
+            'statusCode' => $code
         ], $code);
     }
 
-    /**
-     * The message, if the request was not successful
-     *
-     * @return mixed
-     */
     protected function error($data, $message = null, $code)
     {
         return response()->json([
             'status' => 'Error has occurred...',
             'message' => $message,
-            'data' => $data
+            'data' => $data,
+            'statusCode' => $code
         ], $code);
     }
 }
