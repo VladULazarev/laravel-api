588c0669a7ebf04983fa8e793b01666689ae3927
Lazarev Vladimir <v.lazarev@stmd.pro>
2023-07-12 18:36:58 +0300

    The refactored code improves readability by simplifying some variable assignments

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Traits\HttpResponses;
-use Illuminate\Http\Request;
 use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\LoginUserRequest;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -18,14 +19,15 @@ class AuthController extends Controller
     /**
      * Handle an incoming login request.
      *
-     * @param  App\Http\Requests\LoginUserRequest  $request
-     * @return \Illuminate\Http\Response
+     * @param  LoginUserRequest  $request
+     * @return JsonResponse
      *
-     * @throws \Illuminate\Validation\ValidationException
+     * @throws ValidationException
      */
-    public function login(LoginUserRequest $request)
+    public function login(LoginUserRequest $request): JsonResponse
     {
-        $credentials = ['email' => $request->email, 'password' => $request->password];
+       // $credentials = ['email' => $request->email, 'password' => $request->password];
+        $credentials = $request->only('email', 'password');
 
         if (! Auth::attempt($credentials)) {
             return $this->error('', 'Credentials do not match.', 401);
@@ -33,23 +35,24 @@ class AuthController extends Controller
 
         $user = User::where('email', $request->email)->first();
 
-        $token = $user->createToken('API Token of ' . $user->name);
+        //$token = $user->createToken('API Token of ' . $user->name);
+        $token = $user->createToken('API Token of ' . $user->name)->plainTextToken;
 
         return $this->success([
             'user' => $user,
-            'token' => $token->plainTextToken
+            'token' => $token
         ], 'You have been successfully logged in.');
     }
 
     /**
      * Handle an incoming registration request.
      *
-     * @param  App\Http\Requests\StoreUserRequest  $request
-     * @return \Illuminate\Http\Response
+     * @param  StoreUserRequest  $request
+     * @return JsonResponse
      *
-     * @throws \Illuminate\Validation\ValidationException
+     * @throws ValidationException
      */
-    public function register(StoreUserRequest $request)
+    public function register(StoreUserRequest $request): JsonResponse
     {
         $user = User::create([
             'name' => $request->name,
@@ -57,20 +60,20 @@ class AuthController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
-        $token = $user->createToken('API Token of ' . $request->name);
+        $token = $user->createToken('API Token of ' . $request->name)->plainTextToken;
 
         return $this->success([
             'user' => $user,
-            'token' => $token->plainTextToken
+            'token' => $token
         ], 'You have been successfully registered.');
     }
 
     /**
      * Handle an incoming logout request.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
-    public function logout()
+    public function logout(): JsonResponse
     {
         Auth::user()->currentAccessToken()->delete();
 
